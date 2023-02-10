6fab330b9ac55ee2e1a99c77b7352dad9dc54871
F-19-F <58457605+F-19-F@users.noreply.github.com>
2023-02-10 14:42:38 +0800

    selinux: Generalize support for NNP/nosuid SELinux domain transitions

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -74,6 +74,7 @@ enum {
 	POLICYDB_CAPABILITY_OPENPERM,
 	POLICYDB_CAPABILITY_REDHAT1,
 	POLICYDB_CAPABILITY_ALWAYSNETWORK,
+	POLICYDB_CAPABILITY_NNP_NOSUID_TRANSITION,
 	__POLICYDB_CAPABILITY_MAX
 };
 #define POLICYDB_CAPABILITY_MAX (__POLICYDB_CAPABILITY_MAX - 1)
@@ -83,6 +84,7 @@ extern int selinux_android_netlink_getneigh;
 extern int selinux_policycap_netpeer;
 extern int selinux_policycap_openperm;
 extern int selinux_policycap_alwaysnetwork;
+extern int selinux_policycap_nnp_nosuid_transition;
 
 /*
  * type_datum properties
